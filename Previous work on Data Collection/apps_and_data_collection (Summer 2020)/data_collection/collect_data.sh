e24ff7cb3b4f2ce0f2be047b7f5a0d0a8f5a5b5a
Littsburgh <cmt101@pitt.edu>
2020-10-05 16:57:13 -0400

    Testing export statements

--- Previous work on Data Collection/apps_and_data_collection (Summer 2020)/data_collection/collect_data.sh
+++ Previous work on Data Collection/apps_and_data_collection (Summer 2020)/data_collection/collect_data.sh
@@ -5,6 +5,21 @@
 
 cd scripts
 
+#Make export statements to clean up the .. notation
+export APP_BIN_DIR=($PWD)"/../app/$1/bin"
+export APP_PMC_DIR=($PWD)"/../app/$1/pmc"
+export APP_OUT_DIR=($PWD)"/../app/$1/outlier_dir"
+
+export COLLECTION_SCRIPTS_PATH=($PWD)"/scripts"
+export COLLECTOR_PATH=($PWD)"/bin"
+
+echo $APP_BIN_DIR
+echo $APP_PMC_DIR
+echo $APP_OUT_DIR
+echo $COLLECTION_SCRIPTS_PATH
+echo $COLLECTOR_PATH
+
+
 sudo ./collect_initial.sh $1
 sudo ./fix_outliers_loop.sh $1 $2
 sudo ./recollect_bad_binaries.sh $1
